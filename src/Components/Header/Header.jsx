4f14813ce359ffd4adfcacded931cadebe9451c4
ahmedsami019 <easami019192@gmail.com>
2025-12-10 00:11:23 +0600

    added date by date-fns

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from "../../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-3 mt-6'>
             <img classNam= "w-[300px]" src={Logo} alt="this is logo" />
             <p className='text-gray-500'>Journalism Without Fear or Favour</p>
+            <p className='text-2xl font-medium'>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
         </div>
     );
 };
