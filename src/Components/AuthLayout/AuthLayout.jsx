bd1034c19da468130b3ef91851c7bfb62944cb56
ahmedsami019 <easami019192@gmail.com>
2025-12-11 20:18:56 +0600

    update the page

--- src/Components/AuthLayout/AuthLayout.jsx
+++ src/Components/AuthLayout/AuthLayout.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
-import { Outlet } from 'react-router';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import { Outlet } from "react-router";
 
 const AuthLayout = () => {
-    return (
-        <div className='w-11/12 mx-auto my-5'>
-            <Navbar></Navbar>
-            {/* this is dynamic children loader section */}
-            <div>
-                <Outlet></Outlet>
-            </div>
+  return (
+    <div className=" bg-base-200 min-h-screen">
+      <div className="w-11/12 mx-auto my-5">
+        <Navbar></Navbar>
+        {/* this is dynamic children loader section */}
+        <div className="flex justify-around">
+          <Outlet></Outlet>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-    return (
-        <div>
-            this is login page
+  return (
+    <div className="flex justify-center items-center min-h-screen w-6/12">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+        <h2 className="text-2xl font-semibold text-center">
+          Login your account{" "}
+        </h2>
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+          <p className="text-center font-medium mt-5">Don’t Have An Account ? <Link to="/auth/register" className="text-secondary underline">Register</Link></p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
