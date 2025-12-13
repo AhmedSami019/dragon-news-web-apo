3fe3c536d52f5a23303d2dab87938f421dd86ee8
ahmedsami019 <easami019192@gmail.com>
2025-12-13 22:33:17 +0600

    manage app to seed user status

--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 import app from "../../firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
+  const [user, setUser] = useState(null);
 
-    const auth = getAuth(app)
-    const [user, setUser] = useState(null)
+  const registerUSer = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
 
-    const registerUSer = (email, password)=>{
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+//   to make sure is user logged in or not
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
-//   const user = { name: "sami", email: "sami@jami.com" };
+  //   const user = { name: "sami", email: "sami@jami.com" };
   const authData = {
     user,
     setUser,
-    registerUSer
+    registerUSer,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
