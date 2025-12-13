9b3b62903569b0ad9ed6dbcdc781afee79c1f4fc
ahmedsami019 <easami019192@gmail.com>
2025-12-13 22:23:49 +0600

    added firebase authentication for email & password resister

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import AuthContext from "../../Providers/AuthProvider/AuthContext";
 
 const Register = () => {
+  const { registerUSer, setUser } = use(AuthContext);
 
-  const handleRegister = e =>{
-    e.preventDefault()
-    const name = e.target.name.value
-    const photo = e.target.photo.value
-    const email = e.target.email.value
-    const password = e.target.password.value
+  // register handle function
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
 
-    console.log({name, photo, email, password});
-
-  }
+    console.log({ name, photo, email, password });
+    registerUSer(email, password)
+      .then((result) => {
+        const newUser = result.user;
+        console.log(newUser);
+        setUser(newUser);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+      });
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen w-6/12">
@@ -24,20 +36,46 @@ const Register = () => {
           <form onSubmit={handleRegister} className="fieldset">
             {/* email */}
             <label className="label">Your Name</label>
-            <input type="text" className="input" name="name" placeholder="your name" />
+            <input
+              type="text"
+              className="input"
+              name="name"
+              placeholder="your name"
+              required
+            />
 
             {/* email */}
             <label className="label">Photo URL</label>
-            <input type="text" className="input" name="photo" placeholder="photo url" />
+            <input
+              type="text"
+              className="input"
+              name="photo"
+              placeholder="photo url"
+              required
+            />
 
             {/* email */}
             <label className="label">Email</label>
-            <input type="email" className="input" name="email" placeholder="Email" />
+            <input
+              type="email"
+              className="input"
+              name="email"
+              placeholder="Email"
+              required
+            />
 
             {/* password */}
             <label className="label">Password</label>
-            <input type="password" className="input" name="password" placeholder="Password" />
-            <button type="submit" className="btn btn-neutral mt-4">Register</button>
+            <input
+              type="password"
+              className="input"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
           </form>
           <p className="text-center font-medium mt-5">
             Already Have An Account ?{" "}
--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import AuthContext from "./AuthContext";
+import app from "../../firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
 
+    const auth = getAuth(app)
     const [user, setUser] = useState(null)
+
+
+    const registerUSer = (email, password)=>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
 //   const user = { name: "sami", email: "sami@jami.com" };
   const authData = {
     user,
-    setUser
+    setUser,
+    registerUSer
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
